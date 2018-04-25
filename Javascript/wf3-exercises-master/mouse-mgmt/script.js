c5f827c2a39401e7c66c811eca248a0257c1a49c
Fabious <fabious@free.fr>
2018-04-25 14:12:37 +0200

    mercredi 25/04 aprem

--- Javascript/wf3-exercises-master/mouse-mgmt/script.js
+++ Javascript/wf3-exercises-master/mouse-mgmt/script.js
@@ -1,4 +1,30 @@
 // once the tree DOM has loaded
 document.addEventListener('DOMContentLoaded', _ => {
-  // your logic
+
+  const btn = document.querySelector('#toggle-rectangle');
+  const rectangle = document.querySelector('.rectangle');
+
+
+  btn.addEventListener("click", e => {
+    if (rectangle.style.display === 'block') {
+      rectangle.style.display = 'none';
+    } else {
+      rectangle.style.display = 'block';
+    }
+  });
+
+  rectangle.addEventListener("mouseover", function () {
+    rectangle.classList.toggle("important");
+  });
+
+  rectangle.addEventListener("mouseout", function () {
+    rectangle.classList.toggle("important");
+  });
+
+  rectangle.addEventListener("dblclick", function () {
+    rectangle.classList.toggle("good");
+  });
+
+  
+  
 });
--- Javascript/wf3-exercises-master/photos-selector/script.js
+++ Javascript/wf3-exercises-master/photos-selector/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', _ => {
-  // your logic
+  const photos = document.querySelectorAll(".photo-list li");
+
+  photos.forEach(element => {
+    element.addEventListener("mouseenter", () => {
+      element.classList.add("highlight");
+    });
+    element.addEventListener("mouseleave", () => {
+      element.classList.remove("highlight");
+    });
+    element.addEventListener("click", () => {
+      element.classList.toggle("selected");
+      const total = document.querySelectorAll(".photo-list li.selected").length;
+      document.querySelector("em").innerHTML = total;
+    });
+  });
+  
 });
